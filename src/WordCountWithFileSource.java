99f757bd7f8ad4d9af898804a78e9cf3bcbf2f84
knil-sama <cols.knil@gmail.com>
2014-05-04 22:10:39 +0200

    correction
    remove useless import, add IntWritable and FileSplit in the import list
    
    add exception InterruptedException on function reduce and map
    correct the typo error on FileAndWord
    
    remplace Iterator values by Iterable values in reduce and update the
    loop according to the change

--- src/WordCountWithFileSource.java
+++ src/WordCountWithFileSource.java
@@ -3,12 +3,12 @@ package src;
 import java.io.IOException;
 import java.util.StringTokenizer;
 
-import java.io.IOException;
-import java.util.ArrayList;
 import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 
+import org.apache.hadoop.mapreduce.FileSplit;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.Reducer;
@@ -19,8 +19,8 @@ import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 public class WordCountWithFileSource {
 	public static class Map extends Mapper<LongWritable, Text, Text, IntWritable> {
 		private final static IntWritable one = new IntWritable(1);
-		private Text fileAndWorld = new Text();
-		public void map(LongWritable key, Text value, Context context) throws IOException{
+		private Text fileAndWord = new Text();
+		public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException{
 			String line = value.toString();
 			StringTokenizer tokenizer = new StringTokenizer(line);
 			FileSplit fileSplit = (FileSplit)context.getInputSplit();
@@ -33,10 +33,10 @@ public class WordCountWithFileSource {
 		}
 	}
 	public static class Reduce extends Reducer<Text, IntWritable, Text, IntWritable> {
-		public void reduce(Text key, Iterator<IntWritable> values, Context context) throws IOException {
+		public void reduce(Text key, Iterator<IntWritable> values, Context context) throws IOException, InterrupteException {
 			int sum = 0;
-			while(values.hasNext()){
-				sum += values.next().get();
+			for(IntWritable value : values){
+				sum += value.get();
 			}
 			context.write(key, new IntWritable(sum));
 		}
@@ -45,7 +45,7 @@ public class WordCountWithFileSource {
 	public static void main(String[] args) throws Exception{
 		Job job = new Job();
 		job.setJarByClass(WordCount.class);
-		job.setJobName("Word Count;
+		job.setJobName("Word Count With File Source");
 		
 		job.setOutputKeyClass(Text.class);
 		job.setOutputValueClass(IntWritable.class);
