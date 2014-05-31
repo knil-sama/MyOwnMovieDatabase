19ec0c8e305d018e8ef9b20a29705175c706ff08
knil <cols.knil@gmail.com>
2014-05-31 16:11:11 +0200

    final version NumberOccurenceTitle

--- src/NumberOccurenceTitle.java
+++ src/NumberOccurenceTitle.java
@@ -7,57 +7,79 @@ import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
-import org.apache.hadoop.mapred.JobConf;
-import org.apache.hadoop.mapred.MapReduceBase;
-import org.apache.hadoop.mapred.Mapper;
-import org.apache.hadoop.mapred.OutputCollector;
-import org.apache.hadoop.mapred.Reducer;
-import org.apache.hadoop.mapred.Reporter;
-import org.apache.hadoop.mapred.TextInputFormat;
-import org.apache.hadoop.mapred.TextOutputFormat;
 import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 
-
+/**
+ * 
+ * @author clément demonchy this Mapper Reducer count the number of repetition
+ *         for a movie/serie title for imb file, movies.list.tsv
+ * 
+ */
 public class NumberOccurenceTitle {
-	
-	public static class Map extends MapReduceBase implements Mapper<LongWritable,Text, Text, IntWritable>{
-		private final static IntWritable one = new IntWritable(1);
+	// input "title_movie" (année_tournage) titre_episode (#s.e) annee_sortie
+	// ouput "title_movie" 1
+	public static class Map extends
+			Mapper<LongWritable, Text, Text, IntWritable> {
+		
+		private final IntWritable one = new IntWritable(1);
 		private Text title = new Text();
-		public void map(LongWritable key, Text value,OutputCollector<Text, IntWritable> output, Reporter reporter) throws IOException{
-			String line = value.toString();
+
+		public void map(LongWritable key, Text value, Context context)
+				throws IOException, InterruptedException {
+			// we use lower case to not miss title on upper/lower case
+			// difference
+			String line = value.toString().toLowerCase();
 			title.set(line.split("\\(")[0]);
-			output.collect(title, one);
-		}
-	}
-	public static class Reduce extends MapReduceBase implements Reducer<Text, IntWritable,Text, IntWritable>{
-		public void reduce(Text key, Iterator<IntWritable> values, OutputCollector<Text, IntWritable> output, Reporter reporter) throws IOException{
-			int sum = 0;
-			while(values.hasNext()){
-			 sum += values.next().get();
-			}
-			output.collect(key, new IntWritable(sum));
+			context.write(title, one);
 		}
 	}
 
-	public static void main(String[] args) throws Exception{
-		JobConf conf = new JobConf(NumberOccurenceTitle.class);
-		conf.setJobName("NumberOccurenceTitle");
-		
-		conf.setOutputKeyClass(Text.class);
-		conf.setOutputValueClass(IntWritable.class);
-		
-		conf.setMapperClass(Map.class);
-		conf.setReducerClass(Reduce.class);
-			
-		conf.setInputFormat(TextInputFormat.class);
-		conf.setOutputFormat(TextOutputFormat.class);
-		Job job = new Job(conf);
+	// "title_movie" 1
+	// "title_movie" number_of_same_title_movie
+	public static class Reduce extends
+			Reducer<Text, IntWritable, Text, IntWritable> {
+		public void reduce(Text key, Iterable<IntWritable> values,
+				Context context) throws IOException, InterruptedException {
+			int sum = 0;
+			Iterator<IntWritable> iterator = values.iterator();
+			@SuppressWarnings("unused")
+			IntWritable value;
+			while(iterator.hasNext()){
+				value = iterator.next();
+				// sum += value.get();
+				// in this case we already know that the value is 1 so to
+				// optimize we don't try to get the real value
+				sum += 1;
+			}
+			context.write(key, new IntWritable(sum));
+		}
+	}
+
+	public static void main(String[] args) throws Exception {
+		if (args.length != 2) {
+			System.err.println("Usage : Template <source> <destination>");
+			System.exit(-1);
+		}
+		@SuppressWarnings("deprecation")
+		Job job = new Job();
+
+		job.setJarByClass(NumberOccurenceTitle.class);
+		job.setJobName("NumberOccurenceTitle");
+
 		FileInputFormat.addInputPath(job, new Path(args[0]));
 		FileOutputFormat.setOutputPath(job, new Path(args[1]));
-		
+
+		job.setMapperClass(Map.class);
+		job.setReducerClass(Reduce.class);
+
+		job.setOutputKeyClass(Text.class);
+		job.setOutputValueClass(IntWritable.class);
+
 		System.exit(job.waitForCompletion(true) ? 0 : 1);
-		
+
 	}
 }
